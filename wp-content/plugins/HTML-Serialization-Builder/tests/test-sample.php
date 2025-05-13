cc471da9693e1ab440346d4b0fcdc42eae29e1b7
Milana Cap <milanazzap@gmail.com>
2025-05-13 20:49:38 +0200

    Add Serialization builder class by Dennis Snell.

---
+++ wp-content/plugins/HTML-Serialization-Builder/tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package HTML_Serialization_Builder
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
