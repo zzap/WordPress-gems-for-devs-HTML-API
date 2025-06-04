7f0d40b25a853c45b2ed0e8c28d5e8a314207578
Milana Cap <milanazzap@gmail.com>
2025-06-04 11:48:15 +0200

    IPC Berlin session code.

---
+++ wp-content/plugins/wpgems/wpgems.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Plugin Name:     WP Gems
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wpgems
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wpgems
+ */
+
+// Your code starts here.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+add_filter( 'the_content', 'wpgems_remove_nofollow' );
+
+function wpgems_remove_nofollow( string $content ): string {
+
+	$tags = new WP_HTML_Tag_Processor( $content );
+    $home_url_host = parse_url( get_home_url(), PHP_URL_HOST );
+    while ( $tags->next_tag() ) {
+        // find all anchors
+		if ( 'A' === $tags->get_tag() ) {
+			// filter internal links
+            $href = $tags->get_attribute( 'href' );
+
+            if ( str_contains( $href, $home_url_host ) ) {
+				// remove rel attribute
+				$tags->remove_attribute( 'rel' );
+			}
+
+		}
+    }
+
+    // return filtered html
+    return $tags->get_updated_html();
+}
